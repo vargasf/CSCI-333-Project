8bf3bceedba9b9b5ef7b4c0d530920b72847e12c
FERNANDO V <varga051@rangers.uwp.edu>
2023-03-07 00:14:40 -0600

    Rewrote Everything

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-function alertMessage() {
-    alert("Message sent!");
-  }
-  
-  //Date and time
-  function displayDateTime() {
-    const dateTimeDisplay = document.getElementById("dateTime");
-    const currentDateTime = new Date();
-    dateTimeDisplay.innerHTML = currentDateTime.toLocaleString();
-  }
-  
-  displayDateTime();
-  setInterval(displayDateTime, 1000);
+// Add Later
   
