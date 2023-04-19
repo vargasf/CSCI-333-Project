ea8ce5936a50e93aa83a63ae477aef94d93cf889
FERNANDO V <varga051@rangers.uwp.edu>
2023-04-19 15:33:23 -0500

    Fixed bugs on index.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// --------------- portfolio --------------- //
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,4 +28,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// --------------- Index --------------- //
+
+
 
